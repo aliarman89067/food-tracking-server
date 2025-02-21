a5ae65f50deeb3aa888d8688a4efab2ba72bb9d6
ali arman <ali@gmail.com>
2025-02-21 16:50:00 +0500

    Update vercel.json

--- app.js
+++ app.js
@@ -7,12 +7,9 @@ import foodsRouter from "./routes/foods.route.js";
 const app = express();
 
 app.use(express.json());
+app.use("/", cuisinesRouter);
 app.use("/api/v1/cuisines", cuisinesRouter);
 app.use("/api/v1/foods", foodsRouter);
-
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
 
 app.listen(process.env.PORT, () => {
   mongoose.connect(process.env.MONGO_URI).then(() => {
