4cec83d131f00bd03cc6eff1d035a190602ef969
ali arman <ali@gmail.com>
2025-02-21 16:58:02 +0500

    Update vercel.json

--- controllers/cuisines.controller.js
+++ controllers/cuisines.controller.js
@@ -2,10 +2,11 @@ import Cuisine from "../models/cuisine.model.js";
 import Food from "../models/food.model.js";
 
 export const getAllCuisines = async (req, res) => {
-  try {
-    const cuisinesData = await Cuisine.find();
-    res.status(200).json({ success: true, data: cuisinesData });
-  } catch (error) {
-    console.log(error);
-  }
+  res.send("Its Working");
+  // try {
+  //   const cuisinesData = await Cuisine.find();
+  //   res.status(200).json({ success: true, data: cuisinesData });
+  // } catch (error) {
+  //   console.log(error);
+  // }
 };
