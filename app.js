9f05e0c9febea65bc70927de500299b78e157fdb
ali arman <ali@gmail.com>
2025-02-21 16:56:47 +0500

    Update vercel.json

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ import mongoose from "mongoose";
 import "dotenv/config";
 import cuisinesRouter from "./routes/cuisines.route.js";
 import foodsRouter from "./routes/foods.route.js";
+import Cuisine from "./models/cuisine.model.js";
+import Food from "./models/food.model.js";
 
 const app = express();
 
 app.use(express.json());
-app.use("/", cuisinesRouter);
 app.use("/api/v1/cuisines", cuisinesRouter);
 app.use("/api/v1/foods", foodsRouter);
+
+app.get("/", async (req, res) => {
+  try {
+    const cuisinesData = await Cuisine.find();
+    res.status(200).json({ success: true, data: cuisinesData });
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.listen(process.env.PORT, () => {
   mongoose.connect(process.env.MONGO_URI).then(() => {
